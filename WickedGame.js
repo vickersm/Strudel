b359f7b6c9f1724b7e034885817184802daef7fc
Mike Vickers <vghazard@gmail.com>
2026-01-29 15:17:05 -0600

    Refactor WickedGame.js with new arrangements and effects

--- WickedGame.js
+++ WickedGame.js
@@ -1,49 +1,70 @@
+/**
+ * @title Wicked Game (cover)
+ * @by vghazard
+ * @link 
+ */
+setCpm(120/4);
+
+const piano = arrange(
+  [4, note("<1 5 8 5 8 5 1 5>*8").scale("a:major").transpose("<0 -2 -7@2>")],
+  [4, note("<1 3 5 8 10 8 5 1>*8").scale("a:major").scaleTrans("<0 -1 -4@2>")],
+).trans(-12).sound("gm_epiano2:1").vib("<0!4 40:.6>/4");
 $: arrange(
-  [4, note("<1 5 8 5 8 5 1 5>*8").scale("a:major").transpose("<0 -2 -7 _>")],
-  [4, note("<1 3 5 8 10 8 5 1>*8").scale("a:major").scaleTrans("<0 -1 -4 _>")],
-).trans(-12).sound("gm_epiano2:1").vib("<0!4 40:.8>/4")
+  [8, piano],
+  [4, piano.delay("<1 0>").delaysync(.75).delaytime(1)],
+);
+  
 
 $: arrange(
   [4, silence],
   [40, note("<1 5 3 1 4 3 1 3 0 4 2 0 3 2 1 0>*8").scale("a:major")],
-).s("gm_pad_poly:<0!16 3!4>").pan(rand).gain(slider(1,.5,2,.1))
+).s("gm_pad_poly:<0!16 3!4>").pan(rand2).gain(slider(1,.5,2,.1))
 
 $: arrange(
   [4, silence],
-  [8, note("[1]")],
+  [8, note("<[1@3 1] 1!2>*2").delay(.5)],
+  [4, note("[1,5,8,12,15]").every(2, x => x.jux(rev)).arp("[0 1 2 3 [0,2,4] 3 2 1]")],
+  [4, note("[1(3,8)]")],
   [8, note("[1(3,8) 1(2,4)]").room(0.7).decay(0.8)],
-  [12, note("[1(3,8)]")],
-).scale("a:major").scaleTrans("<0 -1 -4 _>").trans(-24).sound("gm_acoustic_bass:<3 4>/4").gain("<1 1 1.5 _>")._pianoroll()
+  [4, note("[1(3,8)]")],
+  [12, note("[1(3,8) 1(2,4)]").room(0.7).decay(0.8)],
+).scale("a:major").scaleTrans("<0 -1 -4@2>").trans(-24).sound("gm_acoustic_bass:<3 4>/4").gain("<1 1.2 1.4@2>")._pianoroll()
 
 $: arrange(
   [16, silence],
   [40, note("[1 3 5 8 10 <12 8> <15 5> <17 ~>]*2").scale("a:major").scaleTrans("<0 -1 -4 _>")],
-).trans(-12).sound("gm_epiano2:<0!2 2 3!2 5!2 6 7 8>/4").pan(rand2).room(0.8).vib("<0!18 20!2>")
+).trans(-12).sound("gm_epiano2:<0!2 2 3!2 5!2 6 7 8>/4").pan(rand2).room(0.8).vib("<0!19 20>")
 
-const melody = n(`<[-@3 [3 3] 3 3@2 2@3 2 1@8 1!2 1@2 1@2 2@2 1@4 -]
-        [-@3 [3 3] 3 3@2 4@3 4 2@8 4!2 4@2 5@2 4@2 3@3 1@2]
-        [5@6 4@4 3@1 2@1 3@0.5 2@0.5 1@8 - 1@2]
-        [5@6 4@4 3@1 2@1 3@0.5 2@0.5 1@8 - 1@2]
+const melody = n(`<[-@3 [3 3] 3 3@2 2@3 2 1@8 1!2 1@2 1@2 2@2 1@5]
+        [-@3 [3 3] 3 3@2 4@3 [4 3] 2@8 4!2 4@2 5@2 4@2 3@3 1@2]
+        [5@6 4@4 3@1 2@1 1@10 - 1@2]
+        [5@6 4@4 3@1 2@1 [3 2] 1@8 - 1@2]
         [5@6 4@4 3@2 2@1 1@8 -@3 2@0.8 1@0.8]
-      >/4`).penv("<[0@16] [0@13 -5@3] [5@2 0@12 -5@2] [5 0@15] [5@2 0@14]>/4");
-const refrain = n(`<[-@3 [3 3] 3 3@2 2@3 2 1@8 1!2 1@2 1@2 2@2 1@4 -]
-        [-@3 [3 3] 3 3@2 4@3 4 2@8 4!2 4@2 5@2 4@2 3@3 1@2]
-        [5@6 4@4 3@1 2@1 3@0.5 2@0.5 1@8 - 1@2]
-        [[5@2 4 4]@4 3@2!2 2@1 3@0.5 2@0.5 1@8 - 1@2]
-        [5@6 4@4 3@2 2@1 1@8 -@3 2@0.8 1@0.5 -@0.3]
-      >/4`);
-const bridge = n("<1 5 2 5 3 5 4 5>*8").fit().mask(`<
+      >/4`).penv("<[0@16] [0@4 [0 1] 0@4 2!2 -5@2] [5@2 0@12 -5@2] [5 0@15] [5@2 0@14]>/4");
+const refrain = n(`<
+        [- 5 3@2 4 3 2 1]!3
+        [- 0 1@2 2 3 2 1]
+        [- 1 1@2 1@2 1 1]!0
+      >`).pan(sine2);
+const bridge = n("<<1 2 3 4> 5>*8").fit().mask(`<
       <[1 0 1 0 1 0 1 0] 
        [1 1 1 1 1 1 1 1]>
        [0 1 0 1 0 1 0 1] 
-      >`).gain("<0.7 0.8 0.9 0.8 0.7>").room(.5);
+      >`).gain("[0.7 0.9 1.1 0.8 0.6]/8").room(.5);
 $: arrange(
   [20, silence],
   [20, melody],
   [4, silence],
-  [16, bridge],
+  [12, bridge],
+  [8, refrain],
 ).scale("a:major").sound("gm_electric_guitar_clean")
 
+/*
+1234567890123456.
+1 2 3 4 5 6 7 8 .
+1   2   3   4   .
+   -- - - -  - -                 
+*/
 const beat1 = stack(
     s("bd*4").gain(0.7),                // Four-on-the-floor kick drum
     s("~ sd").room(0.5),      // Snare on the backbeat with reverb
