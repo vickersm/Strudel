4d2a48d3845f8790f8c9ec5c631c97c475640d3e
Mike Vickers <vghazard@gmail.com>
2026-01-30 12:55:53 -0600

    Refactor WickedGame.js to improve arrangement

--- WickedGame.js
+++ WickedGame.js
@@ -1,8 +1,3 @@
-/**
- * @title Wicked Game (cover)
- * @by vghazard
- * @link 
- */
 setCpm(100/4);
 
 const piano = arrange(
@@ -18,8 +13,8 @@ $: arrange(
 
 $: arrange(
   [4, silence],
-  [40, note("<1 5 3 1 4 3 1 3 0 4 2 0 3 2 1 0>*8").scale("a:major")],
-).s("gm_pad_poly:<0!16 3!4>").pan(rand2).gain(slider(0.8,.5,2,.1))
+  [40, note("<1 5 3 1 4 3 <1 2> <3 1>>*8").scale("a:major").scaleTrans("<0 -1>")],
+).s("gm_pad_poly:<0!16 3!4>").pan(rand2).gain(slider(0.7,.5,2,.1))
 
 $: arrange(
   [4, silence],
@@ -42,8 +37,9 @@ const melody = n(`<[-@3 [3 3] 3 3@2 2@3 2 1@8 1!2 1@2 1@2 2@2 1@5]
         [5@6 4@4 3@1 2@1 1@10 - 1@2]
         [5@6 4@4 3@1 2@1 [3 2] 1@8 - 1@2]
         [5@6 4@4 3@2 2@1 1@8 -@3 2@0.8 1@0.8]
-        <[-@16] [-@7 [2 1] -@7 [2 1]]>
-      >/4`).gain(1.5).penv("<[0@16] [0@4 [0 1] 0@4 2!2 -4@2] [5@2 0@12 -3@2] [5 0@15] [5@2 0@14] [0@16]>/4")
+        <[-@16]
+         [-@7 [2 1] -@7 [2 1]]>
+      >/4`).gain(1.5).penv("<[0@16] [0@4 [0 1] 0@4 2 0 -4@2] [5@2 0@12 -3@2] [5 0@15] [5@2 0@14] [0@16]>/4")
             // .vib(`<
             //         [0@7 6@3 0 5@8 0@11 4@4]
             //         [0@4 [0 1] 0@4 2!2 -5@2]
@@ -55,11 +51,11 @@ const bridge = n("<<1 2 3 4> 5>*8").fit().mask(`<
       <[1 0 1 0 1 0 1 0] 
        [1 1 1 1 1 1 1 1]>
        [0 1 0 1 0 1 0 1] 
-      >`).gain("[1 1.1 1.2 1 0.8]/8").room(1.5));
+      >`).gain("[1 1.1 1.2 1 0.8]/8").room(1.5);
 const refrain = n(`<
         [- 5 3@2 4 3 2 1]!3
         [- <0 1> 1@2 <2 1> <3 1> <2 1> 1]
-      >`).pan(sine2).room(.7);
+      >`).pan(sine2).room(.7).delay(.2);
 $: arrange(
   [20, silence],
   [24, melody.scale("a:major").sound("gm_acoustic_guitar_nylon:9")],
@@ -67,12 +63,6 @@ $: arrange(
   [8, refrain.scale("a:major").sound("gm_electric_guitar_clean:3")],
 )
 
-/*
-1234567890123456.
-1 2 3 4 5 6 7 8 .
-1   2   3   4   .
-   -- - - -  - -                 
-*/
 const beat1 = stack(
     s("bd*4").gain(0.7),                // Four-on-the-floor kick drum
     s("~ sd").room(0.5),      // Snare on the backbeat with reverb
